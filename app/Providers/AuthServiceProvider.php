eb641d81163509c7e8f8f58077f87169913cbf47
Shaon Majumder <smazoomder@gmail.com>
2025-02-08 18:45:03 +0600

    get access-token from httpCookie

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Http\Request;
+use Laravel\Sanctum\Sanctum;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Sanctum::$accessTokenRetrievalCallback = function (Request $request) {
+            return $request->cookie('access_token');
+        };
     }
 }
